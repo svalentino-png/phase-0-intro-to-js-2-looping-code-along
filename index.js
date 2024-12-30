8b865ad8362f820470176ea2a746f1f34376d2b8
Sam Valentino <sammyvalentino7@gmail.com>
2024-12-30 11:52:23 -0500

    Completed looping code

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
